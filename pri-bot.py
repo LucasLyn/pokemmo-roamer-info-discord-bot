44c354d9497cd6d2d637aca081e1f3fa2b8fc434
LucasLyn <lucaslyn1@gmail.com>
2024-06-09 01:14:47 +0200

    Added /roamers command

--- pri-bot.py
+++ pri-bot.py
@@ -32,5 +32,31 @@ async def on_ready():
     print(f'Successfully logged in as {bot.user.name} ({bot.user.id})')
 
 
+@bot.slash_command(name="roamers",
+                   description="Get the currently active roamers in each region")
+async def roamers(ctx,
+                  month:int = current_month,
+                  show_next_roamers:bool = True):
+    # Ensure given month is within bounds
+    if month != current_month:
+        month = month if month in range(1, 12) else 1 if month < 1 else 12
+
+    # Set current and next month values and names
+    month_name = month_names[month]
+    next_month = month+1 if month < 12 else 1
+    next_month_name = month_names[next_month]
+
+    # Roamers current month string
+    curr_roamers = get_avail_roamers_in_month(all_roamers, month)
+    curr_roamers_str = get_avail_roamers_str(curr_roamers, prefix_str=f"Currently available roamers ({month_name})")
+
+    # Roamers next month string
+    next_roamers_str = '' # init empty to void problems with .join()
+    if show_next_roamers:
+        next_roamers = get_avail_roamers_in_month(all_roamers, next_month)
+        next_roamers_str = get_avail_roamers_str(next_roamers, prefix_str=f"Next available roamers ({next_month_name})")
+    
+    await ctx.send(''.join([curr_roamers_str, next_roamers_str]))
+
 
 bot.run(TOKEN)
