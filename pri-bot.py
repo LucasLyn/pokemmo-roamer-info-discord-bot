788af942aba0d184ef0b9e19170b0d82396c5000
LucasLyn <lucaslyn1@gmail.com>
2024-06-09 01:13:04 +0200

    Add proper submodule imports and barebones bot functionality

--- pri-bot.py
+++ pri-bot.py
@@ -1,9 +1,36 @@
 import os # Used to load the token environment variable
+import sys # Used to insert the sys path at runtime
+import importlib # Used to import unconventinally named submodules 
 import disnake # Used for discord bot functionality
 from disnake.ext import commands
 from dotenv import load_dotenv # Used to load .env files
+
+# Set sys path to pokemmo-roamer-info to properly import in submodule
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'pokemmo-roamer-info'))
+
+# Dirty pokemmo-roamer-info submodule imports
+init_module = importlib.import_module("pokemmo-roamer-info.__init__")
+pri_module = importlib.import_module("pokemmo-roamer-info.pri")
+
+current_month = getattr(init_module, "current_month")
+month_names = getattr(init_module, "month_names")
+
+get_avail_roamers_in_month = getattr(pri_module, "get_avail_roamers_in_month")
+get_avail_roamers_str = getattr(pri_module, "get_avail_roamers_str")
+all_roamers = getattr(pri_module, "all_roamers")
+
+
 
 # Load the discord bot token
 load_dotenv("token.env")
 TOKEN = os.getenv('BOT_TOKEN')
 
+bot = commands.Bot()
+
+@bot.event
+async def on_ready():
+    print(f'Successfully logged in as {bot.user.name} ({bot.user.id})')
+
+
+
+bot.run(TOKEN)
